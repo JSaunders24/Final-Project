9fd030ea419a3fd973d3b3ea6a6561c7bd566f9b
JSaunders24 <jsaundersx24@gmail.com>
2021-11-07 11:45:36 -0800

    new changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,6 @@
 package com.company;
 
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -24,6 +23,8 @@ public class Main {
          String[] nameArray = new String[1];
          String[] genreArray = new String[1];
          String[] lengthArray = new String[1];
+         Integer hours;
+
 
 
        System.out.println("John Saunders Final Project: Backlogger \n");
@@ -41,17 +42,39 @@ public class Main {
         System.out.println("Game Name: " + gameName + " \n");
 
         System.out.println("Enter the game's genre: ");
-        genre = input.next();
+        genre = scan.nextLine();
         genreArray[0] = genre;
         System.out.println("Name: " + gameName + " Genre: " + genre + " \n");
 
-        System.out.println("Enter the length of the game (Short, Medium, Long, Very Long) \n");
-        gameLength = scan.nextLine();
-        lengthArray[0] = gameLength;
-        System.out.println("Name: " + gameName + " Genre: " + genre + " Length: " + gameLength + " \n");
+        System.out.println("Enter the length of the game (use an integer for estimated number of hours needed for completion) \n");
+        hours = Integer.valueOf(input.next());
+
+        while (hours < 0){
+         System.out.println("Error. Cannot enter a number less than 0. Please choose a number greater than 0");
+         hours = Integer.valueOf(input.next());
+        }
+
+        System.out.println("Hours needed: " + hours + " \n");
+
+        if (hours >= 1 && hours <= 10){
+         lengthArray[0] = "Short";
+        }
+        else if (hours > 10 && hours <= 20){
+         lengthArray[0] = "Medium";
+        }
+        else if (hours > 20 && hours <= 35){
+         lengthArray[0] = "Long";
+        }
+        else if (hours > 35){
+         lengthArray[0] = "Very Long";
+        }
+
+//        gameLength = scan.nextLine();
+//        lengthArray[0] = gameLength;
+//        System.out.println("Name: " + gameName + " Genre: " + genre + " Length: " + gameLength + " \n");
 
         System.out.println("Array Test \n");
-        System.out.println("Name: " + nameArray[0] + " Genre: " + genreArray[0] + " Length: " + lengthArray[0] + " \n");
+        System.out.println("Name: " + nameArray[0] + " \n" + "Genre: " + genreArray[0] + " \n" + "Length: " + lengthArray[0] + " \n");
 
     }
 }
